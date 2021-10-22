4786b883f23883304050a96fc7f69b536ee7d991
Nefi-B <nefimorais01@gmail.com>
2021-10-22 12:50:57 +0530

    first commit

---
+++ app/src/main/java/com/example/fragment/FirstFragment.kt
@@ -0,0 +1,9 @@
+package com.example.fragment
+
+
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment: Fragment(R.layout.fragment_first){
+
+}
---
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fragment
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnFragment1 = findViewById<Button>(R.id.btnFragment1)
+        val btnFragment2 = findViewById<Button>(R.id.btnFragment2)
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+    }
+}
